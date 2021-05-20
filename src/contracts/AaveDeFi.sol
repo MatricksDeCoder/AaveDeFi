65081af327d89d6166d9a15ec13f835bd5ee7864
MatricksDeCoder <infinitumcubed@gmail.com>
2021-05-20 15:28:49 +0200

    errors fix

--- src/contracts/AaveDeFi.sol
+++ src/contracts/AaveDeFi.sol
@@ -18,7 +18,7 @@ contract AaveDeFi {
 
     constructor() {
         // Retrieve LendingPool address using Aave Lending Pool Provider V2
-        provider = ILendingPoolAddressesProvider(address(0xb53c1a33016b2dc2ff3653530bff1848a515c8c5#code)); 
+        provider = ILendingPoolAddressesProvider(address(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5)); 
         lendingPool = ILendingPool(provider.getLendingPool());
     }
 
@@ -29,15 +29,9 @@ contract AaveDeFi {
         // Input variables 
         address daiAddress = address(0x6B175474E89094C44Da98b954EedeAC495271d0F); // DAI mainnet address
         address ethAddress = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE); // ETH mock address
-        uint256 referall = 0; // referralCode 
+        uint16 referall = 0; // referralCode 
         uint256 variableRate = 2; // 1 is stable rate, 2 is variable rate
-        uint256 LTV = 0.8; // LoanToValue Ratio DAI is 0.75 ETH is 0.80 in v2 
-
-        function userDepositDai(uint256 _amountInDai) external {
-        userDepositedDai[msg.sender] = _amountInDai;
-        require(dai.transferFrom(msg.sender, address(this), _amountInDai), "DAI Transfer failed!");
-        aaveLendingPool.deposit(address(dai), _amountInDai, 0);
-    }
+        // LoanToValue Ratio DAI is 0.75 ETH is 0.80 in v2 
 
         // function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode)
         // Deposit the ETH sent with msg.value
@@ -45,7 +39,7 @@ contract AaveDeFi {
         lendingPool.deposit(ethAddress, msg.value, onBehalf, referall);
 
         // Enable deposit to be used as collateral
-        lendingPool.setUserUseReserveAsCollateral(ethAddress, true);
+        //lendingPool.setUserUseReserveAsCollateral(ethAddress, true);
 
         // determine the maximum amount of DAI that can be borrowed against the ETH collateral deposited 
         // function getUserAccountData(address user)
@@ -59,7 +53,7 @@ contract AaveDeFi {
         // use availableBorrowsETH to determin max amount DAI to borrow
         // not sure (making assumptions here) e.g 
         // do we need to use Oracle? Is there functions to determine amount for each asset etc ???
-        uint _amountDAIBorrow = availableBorrowsETH * 3000 // simplify price ETH get amount DAI in wei 
+        uint _amountDAIBorrow = availableBorrowsETH * 3000; // simplify price ETH get amount DAI in wei 
 
         //function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
         // Borrow DAI
@@ -70,3 +64,6 @@ contract AaveDeFi {
 
 }
 
+
+
+ 
